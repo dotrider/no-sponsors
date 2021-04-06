fe87fc0c229cd77a24335dc260d6f97efc626897
dotrider <dotrider883@gmail.com>
2021-04-05 22:41:34 -0700

    Update firebase config

--- src/api/fire.js
+++ src/api/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 let firebaseConfig = {
-    apiKey: env.REACT_APP_FIRE_API_KEY,
-    authDomain: env.REACT_APP_FIRE_AUTH_DOMAIN,
-    projectId: env.REACT_APP_FIRE_PROJECT_ID,
-    storageBucket: env.REACT_APP_FIRE_BUCKET,
-    messagingSenderId: env.REACT_APP_FIRE_SENDER_ID,
-    appId: env.REACT_APP_FIRE_APP_ID
+    apiKey: process.env.REACT_APP_FIRE_API_KEY,
+    authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIRE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRE_SENDER_ID,
+    appId: process.env.REACT_APP_FIRE_APP_ID
   };
   // Initialize Firebase
 let fire = firebase.initializeApp(firebaseConfig);
