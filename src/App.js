ba92743215108343d2c3e529ff503f98de10af27
dotrider <dotrider883@gmail.com>
2021-04-06 21:02:54 -0700

    Setting up DB test mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import './App.css';
+import { addPost } from './api/fire';
 
 function App() {
+
+  const [username, setUserName] = useState('');
+  const [body, setBody] = useState('');
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(2);
+
+  const handleSubmit = () => {
+    addPost(userId, username, title, body)
+  }
   return (
     <div className="App">
-    
+      <label>Username</label>
+      <input onChange={e => setUserName(e.target.value)}/>
+      <label>Title</label>
+      <input onChange={e => setTitle(e.target.value)}/>
+      <label>Body</label>
+      <input onChange={e => setBody(e.target.value)}/>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
--- src/api/fire.js
+++ src/api/fire.js
@@ -11,3 +11,37 @@ let firebaseConfig = {
   // Initialize Firebase
 let fire = firebase.initializeApp(firebaseConfig);
 export default fire;
+
+// DB Tables
+
+const db = firebase.database();
+
+const addUser = (userId, username, email, password, profile_picture) => {
+  db.ref('users/' + userId).set({
+    username,
+    email,
+    password,
+    profile_picture
+  });
+};
+
+export const addPost = (userId, username, title, body) => {
+
+  let newPost = {
+    author: username,
+    userId,
+    body,
+    title,
+  };
+  
+  let newPostKey = db.ref().child('posts').push().key;
+
+  let updates = {};
+  updates['/post/' + newPostKey] = newPost;
+  updates['/user-post/' + userId + '/' + newPostKey] = newPost;
+
+  return db.ref().update(updates)
+}
+
+
+
