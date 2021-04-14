468f0e1787ebed7fab29df227d23f4b19d46eaa2
dotrider <dotrider883@gmail.com>
2021-04-13 22:43:36 -0700

    Impliment Merch component with temporary products

---
+++ src/api/store.js
@@ -0,0 +1,46 @@
+//Temporary Data for layout purposes
+
+const products = [
+  {
+    id: 0,
+    product: "GoPro",
+    category: "Camera",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img:
+      "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img (55).jpg",
+    price: 20.0,
+  },
+  {
+    id: 0,
+    product: "GoPro",
+    category: "Camera",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img:
+      "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img (55).jpg",
+    price: 20.0,
+  },
+  {
+    id: 0,
+    product: "GoPro",
+    category: "Camera",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img:
+      "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img (55).jpg",
+    price: 20.0,
+  },
+  {
+    id: 0,
+    product: "GoPro",
+    category: "Camera",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img:
+      "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img (55).jpg",
+    price: 20.0,
+  },
+];
+
+export default products;
