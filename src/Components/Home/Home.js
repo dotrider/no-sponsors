5eb19eeb6a11a781f4a841cebe1410a774f18d0e
dotrider <56666461+dotrider@users.noreply.github.com>
2021-04-14 18:53:18 -0700

    Impliment sidebar & align dashboard (#3)

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Categories from "../Categories/Categories";
+import Sidebar from "../Sidebar/Sidebar";
+import { MDBRow, MDBCol } from "mdbreact";
 
 const Home = () => {
-  return <Categories />;
+  return (
+    <MDBRow>
+      <MDBCol md="8">
+        <Categories />
+      </MDBCol>
+      <MDBCol md="4">
+        <Sidebar />
+      </MDBCol>
+    </MDBRow>
+  );
 };
 
 export default Home;
